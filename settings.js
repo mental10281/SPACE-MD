05f5b1c2cb02fb90700c5d840e7aacedacd5d753
mental10281 <mentalmunda652@gmail.com>
2025-08-13 11:33:12 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,16 +2,16 @@ require('dotenv').config();
 
 module.exports = {
   //Enter your session id here (optional)
-  SESSION_ID: process.env.SESSION_ID || '',
+  SESSION_ID: process.env.SESSION_ID || 'CYPHER-X:~UEsDBBQAAAgIAMkzDVuJIBF4WgQAAHwHAAAKAAAAY3JlZHMuanNvbpVU25KiSBD9l3rVmOYiCEZ0xAAiItqCd92YhwIKLIGCpgoQJ*z3Dezu6XnYne3lqUiKzJN5zsmfgOSYIge1YPQTFCWuIUPdkbUFAiOgV1GEStAHIWQQjICsDaVepgr+fGauxRSyQwoXbh4kWEacXy31ZjqeVWKd8MozuPdBUfkpDv6QULXmCkFUi*IC9tg6j3vLXa6kG3resLTBS4ESc2zwTrMxn8G9ywhxiUlsFmeUoRKmDmpdiMuvwae6HY3NgzTxFlq5UWeOVPcOVTZx9urLbKcmT2LE1+bSPfHJ1+BLobkuBH9PLcuIb7F4lKY3tRisKt7yPXsZZv5eK45k5UzpG3yKY4JCO0SEYdZ+ee6NKfALw0+OeKctRM1TOd2lGJ5Ptr4R7Z053OwVfSVWSzH4GvDQabNXod2G100dwWOKFXp6jXlVDYXFYsaeZpvjBPZ8mejB78Dd8kMryf+ZO2e50cVulmyws*Fkv9CJQOxUGorXpXagY1lPrrcQLneW+cW5i4Enjm8M0qYsLxoVybq94VRy+VDdR8fJa7K7htY6fTm33id8yKryTyiJoB71J*eqe8dyryxxZmkBPyhVR0sLOpti0ozXL73b64vplOopVBXLVVr3yWhmU*3Fq15uuaH7u3nsGMkqLc+vA5wpevz86ChBrR2CEX*vgxLFmLISMpyTLiYIXB*AsF6joETsMV6QXE76ZZ+NUVTPlM20shr14AwvgWFblWmbah1VbTPZ4+E0eAZ9UJR5gChF4RRTlpftAlEKY0TB6K8ffUDQlb0R15UT+T6IcEnZllRFmsPwg9WPjzAI8oqwdUsCozugEoy4zzBiDJOYdnOsCCyDM66RcYaMglEEU4p+dYhKFIIRKyv0y7VGHnaDH6xc1ZrLG9AH2YMQHHarQBD54VAYKgNOGknf6bemywqL4htBDPQBgd1lcIKYwBTXoA*Sx3+iJAmiyqmSxPPKSPreRe+*8HbpQ8QgTikYAcMdN5lCJ6YbnZWFZ1maF2tGrIHP*j6E8kbEKtxexpeTWmS8E5FwRlaOd*CoMc79yeCgbZC8Vno1CT1j+*wPScAIpMmZS8Od5PVsUdwtmsYnVyb3ChVX9gEGmXMYHmY3wi2fpj1zTulmeFmvprIqT43bwlXVINXcRshfy56uXNPDamour7r33FULUY0D9Huxc*aEG8u8kngMvfhaz+XTnPeVVb2Vr5cd1krr5bJK5EHmB9mhrVzncjsNelN1dnY9eNN7lupDTlgmzV4g*tZAzDQuhvYm4YeF0vfVhR*i6pjrXiOMHpvgnaL*YvINd6c37t7*LcX7avkXe+rbw85uBkIET2xNJHF94tbJhJtrsWxd0aQxxSqp3VISt1oK7vcffVCkkEV5mXWbP*Mh6IMyrzr12iTK*1DJ0DhbezduCinTPh2xwRmiDGYFGPFDSeJkWeaHb7fcMi+mkJ47Sa7Uk6h28m61olgzyD4MBrTH44rg*jdQSwECFAMUAAAICADJMw1biSAReFoEAAB8BwAACgAAAAAAAAAAAAAApIEAAAAAY3JlZHMuanNvblBLBQYAAAAAAQABADgAAACCBAAAAAA=',
   
   //Enter your number here for administrative access to the bot
-  BOT_ADMIN: process.env.BOT_ADMIN || '254712345678',
+  BOT_ADMIN: process.env.BOT_ADMIN || '923177278405',
   
   //Bot web server port
   PORT: parseInt(process.env.PORT) || 2605,
   
   //Enter your Github username here (Compulsory unless you have a valid premium key)
-  GITHUB_USERNAME: process.env.GITHUB_USERNAME || '',
+  GITHUB_USERNAME: process.env.GITHUB_USERNAME || 'mental10281',
   
   //Enter your desired bot password here. Users will be asked for this password when they try to connect to your bot via telegram or web
   //Must be numbers only and a total of 8 digits
@@ -26,7 +26,7 @@ module.exports = {
   TELEGRAM_BOT_TOKEN: process.env.TELEGRAM_BOT_TOKEN || '',
   
    //Antidelete and Antiedit functionality, set true to enable and false to disable. (⚠️ This function consumes a lot of memory + storage, only enable if you have enough resources ⚠️)
-   ANTI_DELETE: process.env.ANTI_DELETE || 'false',
+   ANTI_DELETE: process.env.ANTI_DELETE || 'true',
   
   
   //⚠️ Premium users settings ⚠️
